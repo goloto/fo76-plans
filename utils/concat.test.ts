c2da9b0f399ac3293706bbd50646b171689a44bb
Mikhail Goloto <mikhail@goloto.dev>
2024-05-21 15:11:46 +0300

    fixed tests

--- utils/concat.test.ts
+++ utils/concat.test.ts
@@ -2,24 +2,28 @@ import { expect, test, describe, beforeAll } from 'bun:test';
 import { JSDOM } from 'jsdom';
 
 let EN_ALL = '';
+let EN_DOM: JSDOM = new JSDOM();
 let RU_ALL = '';
+let RU_DOM: JSDOM = new JSDOM();
 
 beforeAll(async () => {
   EN_ALL = await Bun.file('./xml/en/_All.xml').text();
+  EN_DOM = new JSDOM(EN_ALL, {contentType: 'text/xml'});
   RU_ALL = await Bun.file('./xml/ru/_All.xml').text();
+  RU_DOM = new JSDOM(RU_ALL, {contentType: 'text/xml'});
 });
 
 describe('Testing concat', () => {
   test('Strings count in english version should be equal to russian version', () => {
-    const enLength = new JSDOM(EN_ALL).window.document.querySelector('Content')?.childNodes.length || 0;
-    const ruLength = new JSDOM(RU_ALL).window.document.querySelector('Content')?.childNodes.length || 0;
+    const enLength = EN_DOM.window.document.querySelector('Content')?.childElementCount || 0;
+    const ruLength = RU_DOM.window.document.querySelector('Content')?.childElementCount || 0;
 
     expect(enLength).toEqual(ruLength);
   });
 
   test('String id\'s in english version should be equal to id\'s in russian version', () => {
-    const enContent = new JSDOM(EN_ALL).window.document.querySelector('Content');
-    const ruContent = new JSDOM(RU_ALL).window.document.querySelector('Content');
+    const enContent = EN_DOM.window.document.querySelector('Content');
+    const ruContent = RU_DOM.window.document.querySelector('Content');
     const enIds = getIdsFromContent(enContent?.children);
     const ruIds = getIdsFromContent(ruContent?.children);
 
@@ -29,7 +33,7 @@ describe('Testing concat', () => {
 
 const getIdsFromContent = (nodes?: HTMLCollection): string[] => {
   if (!nodes) {
-    return []
+    return [];
   }
 
   const result: string[] = [];
@@ -37,13 +41,10 @@ const getIdsFromContent = (nodes?: HTMLCollection): string[] => {
   for (const node of nodes) {
     const id = node.getAttribute('sID');
 
-    if (!id) {
-      continue;
+    if (id) {
+      result.push(id);
     }
-
-    result.push(id);
   }
-
 
   return result;
 }
