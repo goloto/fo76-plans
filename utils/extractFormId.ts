f5722094f5c451221f19d09c02d25664c540c475
Mikhail Goloto <mikhail@goloto.dev>
2025-06-01 17:29:04 +0300

    changed formatting for edid strings

--- utils/extractFormId.ts
+++ utils/extractFormId.ts
@@ -14,21 +14,21 @@ const extractFormId = async () => {
     const string = await file.text();
     const dom = new JSDOM(string, {contentType: 'text/xml'});
     const stringsElementArray = dom?.window?.document?.querySelectorAll('String');
-    const filteredStrings: Element[] = [];
+    const filteredStringElements: Element[] = [];
     const edidArray: string[] = [];
 
     stringsElementArray.forEach((element) => {
       const containBook = element.innerHTML.indexOf('BOOK:FULL') !== -1;
 
       if (containBook) {
-        filteredStrings.push(element.querySelector('EDID') as Element);
+        filteredStringElements.push(element.querySelector('EDID') as Element);
       }
     })
 
-    filteredStrings.forEach((element) => edidArray.push(element.innerHTML));
+    filteredStringElements.forEach((element) => edidArray.push(element.innerHTML));
 
     if (edidArray.length > 0) {
-      const edidString = edidArray.map((edid) => `${edid},\n`);
+      const edidString = edidArray.map((edid) => `"edid|${edid}",\n`);
       const formattedFileName = fileName.replace('_en.xml', '');
   
       Bun.write(`./formId/${formattedFileName}.txt`, edidString);
